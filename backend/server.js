12c1da717b149d5cd6832eb176b159d1a05f48ae
monasefdev <monasef77@gmail.com>
2024-05-16 16:59:48 +0100

    Update backend configuration and refactor tour controller

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 const dotEnv = require('dotenv');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose');
 
 dotEnv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log('DB connection successful!'))
+  .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
 
